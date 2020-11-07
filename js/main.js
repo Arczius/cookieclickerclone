a5d07ef9d30e01cb65e2990fe39e75712116dcd9
Arc <arcvaartmans@gmail.com>
2020-11-07 15:26:42 +0100

    updated some files

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ const DOLLARSIGN = "$";
 function moneyAdd(){
 	money++;
 	var moneyAmount = money + DOLLARSIGN;
-	console.log("you have " + moneyAmount);
-
+	var btn = document.getElementById("eventBtn");
+    btn.innerHTML = "you have " + moneyAmount;
 }
