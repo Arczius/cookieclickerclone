9d36509d4fa609ecc5cb755acecb424dfcd88f0e
Arczius <70692175+Arczius@users.noreply.github.com>
2021-06-05 11:06:02 +0200

    made the style better of the code

--- js/main.js
+++ js/main.js
@@ -27,11 +27,9 @@ var workerCost = 0;
 function workerAmountText() {
 	if (autoWorkers == 0) {
 		workersAmount.innerText = "you have no workers yet, buy some";
-	}
-	else if (autoWorkers == 1) {
+	} else if (autoWorkers == 1) {
 		workersAmount.innerText = "you have " + autoWorkers + " worker";
-	}
-	else {
+	} else {
 		workersAmount.innerText = "you have " + autoWorkers + " workers";
 	}
 
@@ -40,8 +38,7 @@ function workerAmountText() {
 function autoWorkerAdd(amount) {
 	if (workerCost > money) {
 		alert("the worker couldnt be bought");
-	}
-	else {
+	} else {
 		for (i = 0; i != amount; i++) {
 			autoWorkers++;
 		}
@@ -52,8 +49,7 @@ function autoWorkerAdd(amount) {
 function removeMoney(amount) {
 	if (workerCost > money) {
 		alert("you do not have enough money");
-	}
-	else {
+	} else {
 		for (i = 0; i != amount; i++) {
 			money--;
 		}
@@ -91,6 +87,7 @@ function timeoutMoney() {
 		setInterval(moneyAddWorker(), 1000);
 	}
 }
+
 function moneyAddWorker() {
 	money++;
 	timeoutMoney();
